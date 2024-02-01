58f8cf23b29927c772fbfb65823dfc734e3c1ed2
Develiorate <ala.garbaa.pro@gmail.com>
2024-02-01 20:26:52 +0100

    Create constants.ts

---
+++ src/lib/constants.ts
@@ -0,0 +1,10 @@
+export const cookiesName = 'O';
+export const homePagePath = '/';
+export const logoutPath = '/logout';
+
+
+// Web Content Coordinator
+export const wccPath = "/web-content-coordinator";
+export const wccDashboardPath = wccPath + "/dashboard";
+export const wccPagesPath = wccDashboardPath + "/pages";
+export const wccProfilePath = wccDashboardPath + "/profile";
